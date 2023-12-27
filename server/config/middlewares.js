3c2341e6741cb1a6b2fd1301ee0a0dbf0fc01015
BadmaArs <134961738+BadmaArs@users.noreply.github.com>
2023-12-27 18:16:14 +0300

    cors off

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,12 +1,13 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = ({ env }) => {
+  return {
+    settings: {
+      cors: {
+        enabled: true,
+        origin: ["http://5.181.108.131:1337"],
+      },
+    },
+    load: {
+      before: ["cors", "poweredBy", "logger", "responses", "gzip"],
+    },
+  };
+};
