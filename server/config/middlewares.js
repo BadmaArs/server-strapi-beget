8045a08d2174cc9eb84aee194fb102dc88c919c9
BadmaArs <134961738+BadmaArs@users.noreply.github.com>
2023-12-27 20:09:30 +0300

    html add meta 1

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,23 +1,12 @@
 module.exports = [
+  'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: false,
-      headers: '*',
-      origin: ['https://football-club-coursework.vercel.app', 'http://localhost:3001', 'http://5.181.108.131:1337']
-    }
-  },
-  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
-
-
-
-
