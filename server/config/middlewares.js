7fb491fb156bf842c322f44e8a9faf6572b871e9
BadmaArs <134961738+BadmaArs@users.noreply.github.com>
2023-12-27 19:29:39 +0300

    Seems to have solved the problem with cors

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,12 +1,23 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['https://football-club-coursework.vercel.app', 'localhost:3001']
+    }
+  },
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
+
+
+
+
